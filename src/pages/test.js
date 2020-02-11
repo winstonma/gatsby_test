1cde1d62a04710419f4826285de238b48b9aa014
unknown <winstonma@ymail.com>
2020-02-11 15:46:58 +0800

    Added test page

---
+++ src/pages/test.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TestPage = props => {
+  let name = props["*"]
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi {name}</h1>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export default TestPage
